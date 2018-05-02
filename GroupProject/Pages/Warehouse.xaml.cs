3eac78add536acff63aa8125875c37dd8f92ac70
Lothandar <guillaume.letuppe96@gmail.com>
2018-05-02 11:42:01 +0100

    WareHouse Setup

--- GroupProject/Pages/Warehouse.xaml.cs
+++ GroupProject/Pages/Warehouse.xaml.cs
@@ -28,24 +28,29 @@ namespace GroupProject.Pages
 
         private void UserControl_Initialized(object sender, EventArgs e)
         {
-            string query = "Select order.* , supplier.supplierName FROM `order`, `supplier` Where DeliveryDate = '" + DateTime.Today.ToString("yyyy-MM-dd") + "' And supplierNo = supplier.supplierID; ";
+            string query = "Select `order`.* , supplier.supplierName FROM `order`, `supplier` Where DeliveryDate <= '"+DateTime.Today.ToString("yyyy-MM-dd")+"' And supplierNo = supplier.supplierID;";
             List<List<string>> list = DatabaseManagement.SelectQuery(query);
             foreach (List<string> littlelist in list)
             {
                 orderItems.Add(new orderedItem()
                 {
-                    ID = Int64.Parse(littlelist[0])
+                    ID = Int64.Parse(littlelist[0]),
+                    Price = double.Parse(littlelist[1]),
+                    ExpectedDelivery = littlelist[5],
+                    SupplierName = littlelist[7]
+                     
                 });
             }
+            Orders.ItemsSource = orderItems;
         }
     }
     public class orderedItem
     {
+        public string SupplierName { get; set; }
         public Int64 ID { get; set; }
         public double Price { get; set; }
         public string ExpectedDelivery { get; set; }
-        public string DeliveryDate { get; set; }
-        public string SupplierName { get; set; }
+        
 
 }
 }
